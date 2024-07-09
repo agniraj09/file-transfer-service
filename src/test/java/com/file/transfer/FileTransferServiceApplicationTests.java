d53620998f8fc2b7da6e5a5fc37e9e931d688010
Agniraj <126744684+agniraj09@users.noreply.github.com>
2024-07-09 11:19:14 +0530

    Initial Commit

---
+++ src/test/java/com/file/transfer/FileTransferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.file.transfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileTransferServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
